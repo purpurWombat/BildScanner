a999ba06448a24bac714f0b7df8624968fdd8b5d
Okan <businesss187@gmail.com>
2019-05-08 01:06:49 +0200

    Initial commit

---
+++ app/src/main/java/jannikokan/mybildscanner/de/bildscanner/MainActivity.java
@@ -0,0 +1,13 @@
+package jannikokan.mybildscanner.de.bildscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
